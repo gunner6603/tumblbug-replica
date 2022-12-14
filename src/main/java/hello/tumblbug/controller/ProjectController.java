ffbacc64b2c6b24db46434578633f8abe78d5d3c
Geon <gunner6603@snu.ac.kr>
2022-12-14 22:49:21 +0900

    20221214

--- src/main/java/hello/tumblbug/controller/ProjectController.java
+++ src/main/java/hello/tumblbug/controller/ProjectController.java
@@ -19,6 +19,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MaxUploadSizeExceededException;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
