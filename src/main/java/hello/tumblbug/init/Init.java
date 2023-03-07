e6a75be0087f33f64fc608c95834f79c6664ec3b
Geon <gunner6603@snu.ac.kr>
2023-03-07 19:36:15 +0900

    Refactor: 임시 데이터 생성 로직 변경

--- src/main/java/hello/tumblbug/init/Init.java
+++ src/main/java/hello/tumblbug/init/Init.java
@@ -1,10 +1,8 @@
 package hello.tumblbug.init;
 
-import hello.tumblbug.domain.Category;
-import hello.tumblbug.domain.Member;
-import hello.tumblbug.domain.Project;
-import hello.tumblbug.domain.Reward;
+import hello.tumblbug.domain.*;
 import hello.tumblbug.file.UploadFile;
+import hello.tumblbug.repository.CommunityPostRepository;
 import hello.tumblbug.repository.MemberRepository;
 import hello.tumblbug.repository.ProjectRepository;
 import lombok.RequiredArgsConstructor;
@@ -15,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -22,16 +22,26 @@ public class Init {
 
     private final MemberRepository memberRepository;
     private final ProjectRepository projectRepository;
+    private final CommunityPostRepository communityPostRepository;
 
     @GetMapping("/init")
     @Transactional
-    public String init() {
+    public String init() throws InterruptedException {
 
-        for (int i = 1; i <= 100; i++) {
-            Member member = new Member("테스트멤버" + i, "test" + i, "test" + i + "!");
-            member.setInfo("테스트 문자열 테스트 문자열 테스트 문자열 테스트 문자열 테스트 문자열 테스트 문자열 테스트 문자열 테스트 문자열 테스트 문자열 테스트 문자열 테스트 문자열 테스트 문자열 테스트 문자열 테스트 문자열 테스트 문자열 테스트 문자열 테스트 문자열 테스트 문자열 테스트 문자열 테스트 문자열 테스트 문자열 ");
+        //커뮤니티 포스트 게시용 멤버 생성 (4개)
+        List<Member> communityMembers = new ArrayList<>();
+        for (int i = 1; i < 5; i++) {
+            Member member = new Member("커뮤니티멤버" + i, "community" + i, "community" + i + "!");
             memberRepository.save(member);
-            String title = "테스트 프로젝트" + i + " 테스트 프로젝트" + i + " 테스트 프로젝트" + i;
+            communityMembers.add(member);
+        }
+
+        //테스트 멤버 및 프로젝트 생성 (99개씩)
+        for (int i = 1; i < 100; i++) {
+            Member member = new Member("테스트멤버" + i, "test" + i, "test" + i + "!");
+            member.setInfo("테스트멤버" + i +"의 테스트 소개글입니다. 반갑습니다!" );
+            memberRepository.save(member);
+            String title = "테스트 프로젝트" + i + "의 제목입니다.";
             Category category = Category.BOOK;
             switch (i % 3) {
                 case 0:
@@ -44,32 +54,33 @@ public class Init {
                     category = Category.GOODS;
                     break;
             }
-            UploadFile mainImage = new UploadFile("08ec9357-3ad7-4884-9f23-df86949e941d.png");
+            UploadFile mainImage = new UploadFile("test-image-perfume-1.png");
             int targetSponsorship = 1000000;
-            String description = "테스트 프로젝트 내용 테스트 프로젝트 내용 테스트 프로젝트 내용 테스트 프로젝트 내용 테스트 프로젝트 내용 테스트 프로젝트 내용 테스트 프로젝트 내용 테스트 프로젝트 내용 테스트 프로젝트 내용 테스트 프로젝트 내용 테스트 프로젝트 내용";
+            String description = "테스트 프로젝트" + i +"의 내용입니다.\n" + "테스트 프로젝트" + i +"의 내용입니다.\n" + "테스트 프로젝트" + i +"의 내용입니다.\n" + "테스트 프로젝트" + i +"의 내용입니다.\n" + "테스트 프로젝트" + i +"의 내용입니다.\n" + "테스트 프로젝트" + i +"의 내용입니다.\n" + "테스트 프로젝트" + i +"의 내용입니다.\n" + "테스트 프로젝트" + i +"의 내용입니다.\n" + "테스트 프로젝트" + i +"의 내용입니다.\n" + "테스트 프로젝트" + i +"의 내용입니다.\n" + "테스트 프로젝트" + i +"의 내용입니다.\n";
             LocalDateTime deadline = LocalDateTime.now().plusMonths(2);
-            Reward reward1 = new Reward(10000, "리워드1");
-            Reward reward2 = new Reward(20000, "리워드2");
-            Project project = new Project(title, category, member, mainImage, targetSponsorship, description, deadline, reward1, reward2);
-            projectRepository.save(project);
+            Reward reward1 = new Reward(10000, "테스트 프로젝트" + i + "의 리워드1 내용입니다.");
+            Reward reward2 = new Reward(20000, "테스트 프로젝트" + i + "의 리워드2 내용입니다.");
+            Project testProject = Project.builder()
+                    .title(title)
+                    .category(category)
+                    .creator(member)
+                    .mainImage(mainImage)
+                    .targetSponsorship(targetSponsorship)
+                    .description(description)
+                    .deadline(deadline)
+                    .build();
+            testProject.addReward(reward1);
+            testProject.addReward(reward2);
+            projectRepository.save(testProject);
+
+            //테스트 커뮤니티 포스트 생성 (프로젝트당 4개씩)
+            for (int j = 1; j < 5; j++) {
+                Member communityMember = communityMembers.get(j - 1);
+                CommunityPost communityPost = new CommunityPost(testProject, communityMember, "커뮤니티 포스트" + j + "의 내용입니다.\n" + "커뮤니티 포스트" + j + "의 내용입니다.\n" + "커뮤니티 포스트" + j + "의 내용입니다.\n");
+                communityPostRepository.save(communityPost);
+                Thread.sleep(1);
+            }
         }
-
-
-//        Member member = new Member("테스트 멤버", "test", "test");
-//        memberRepository.save(member);
-//
-//        for (int i = 0; i < 10; i++) {
-//            String title = "테스트 프로젝트" + i + " 테스트 프로젝트" + i + " 테스트 프로젝트" + i;
-//            Category category = Category.GOODS;
-//            UploadFile mainImage = new UploadFile("perfume.png", "08ec9357-3ad7-4884-9f23-df86949e941d.png");
-//            int targetSponsorship = 1000000 * (i + 1);
-//            String description = "테스트 프로젝트 내용 테스트 프로젝트 내용 테스트 프로젝트 내용 테스트 프로젝트 내용 테스트 프로젝트 내용 테스트 프로젝트 내용 테스트 프로젝트 내용 테스트 프로젝트 내용 테스트 프로젝트 내용 테스트 프로젝트 내용 테스트 프로젝트 내용";
-//            LocalDateTime deadline = LocalDateTime.now().plusMonths(2);
-//            Reward reward1 = new Reward(10000, "리워드1");
-//            Reward reward2 = new Reward(20000, "리워드2");
-//            Project project = new Project(title, category, member, mainImage, targetSponsorship, description, deadline, reward1, reward2);
-//            projectRepository.save(project);
-//        }
         return "redirect:/";
     }
 }
